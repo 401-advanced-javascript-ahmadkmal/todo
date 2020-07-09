29c8b0e5db99d4c044354d7f48ac68876d6db56c
ahmadkmal <eng.ahmadkmal@gmail.com>
2020-07-09 08:51:47 +0300

    add auth

--- src/app.js
+++ src/app.js
@@ -2,12 +2,20 @@ import React from 'react';
 
 import ToDo from './components/todo/todo-connected';
 import SettingsContext from './components/context/site';
+import LoginContext from './components/context/loginContext'
 export default function App() {
   return (
     <>
-    <SettingsContext>
-      <ToDo />
-    </SettingsContext>
+          <LoginContext>
+        <Header></Header>
+        <Login />
+        <SignUp />
+        <Auth capability="read">
+          <SettingsContext>
+            <ToDo />
+          </SettingsContext>
+        </Auth>
+        </LoginContext>
     </>
   );
 }
